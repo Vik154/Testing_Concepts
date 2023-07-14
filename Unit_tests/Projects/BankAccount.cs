c5e50db43d3e240d2b6bd510a8369e32efe1b2cb
Vik154 <Smorodin_1209@mail.ru>
2023-07-15 01:04:48 +0700

    Реализация Unit-теста из примера MSDN

--- Unit_tests/Projects/BankAccount.cs
+++ Unit_tests/Projects/BankAccount.cs
@@ -1,17 +1,7 @@
-﻿
-namespace BankAccountNS {
-    
+﻿namespace BankAccountNS {
+
     /// <summary> Bank account demo class. </summary>
     public class BankAccount {
-
-        public static void Main() {
-            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
-
-            ba.Credit(5.77);
-            ba.Debit(11.22);
-            Console.WriteLine("Current balance is ${0}", ba.Balance);
-        }
-
         private readonly string m_customerName;
         private double m_balance;
         public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
---
+++ Unit_tests/Projects/Program.cs
@@ -0,0 +1,16 @@
+﻿using BankAccountNS;
+
+namespace Projects {
+    
+    internal class Program {
+
+        public static void Main() {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
+    }
+}
