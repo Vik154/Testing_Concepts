2d917ff6ad2f84ac6080a53035555e57e7de08f7
Vik154 <Smorodin_1209@mail.ru>
2023-07-15 14:14:42 +0700

    Наивный тест типов

---
+++ Unit_tests/Projects/Types.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Projects {
+    
+    public class Types {
+
+        // Проверка типов
+        public sbyte   test_sbyte()   => (sbyte)-2;
+        public byte    test_byte()    => (byte)2;
+        public short   test_short()   => (short)-22;
+        public ushort  test_ushort()  => (ushort)22;
+        public int     test_int()     => (int)78;
+        public uint    test_uint()    => (uint)24u;
+        public long    test_long()    => (long)787;
+        public ulong   test_ulong()   => (ulong)221;
+        public float   test_float()   => (float)2.12;
+        public double  test_double()  => (double)2.11;
+        public decimal test_decimal() => (decimal)1212;
+        public string  test_string()  => "string";
+    }
+}
---
+++ Unit_tests/Tests/TypesTest.cs
@@ -0,0 +1,46 @@
+﻿using Projects;
+
+namespace Unit_Test {
+
+    [TestClass]
+    public class TypesTest {
+
+        private Types myClass = new Types();
+
+        [TestMethod]
+        public void return_string()  { Assert.AreEqual("string", myClass.test_string()); }
+
+        [TestMethod]
+        public void return_byte()    { Assert.IsTrue(myClass.test_byte() is byte); }
+
+        [TestMethod]
+        public void return_sbyte()   { Assert.IsTrue(myClass.test_sbyte() is sbyte); }
+
+        [TestMethod]
+        public void return_short()   { Assert.IsTrue(myClass.test_short() is short); }
+
+        [TestMethod]
+        public void return_ushort()  { Assert.IsTrue(myClass.test_ushort() is ushort); }
+
+        [TestMethod]
+        public void return_int()     { Assert.IsTrue(myClass.test_int() is int); }
+
+        [TestMethod]
+        public void return_uint()    { Assert.IsTrue(myClass.test_uint() is uint); }
+
+        [TestMethod]
+        public void return_long()    { Assert.IsTrue(myClass.test_long() is long); }
+
+        [TestMethod]
+        public void return_ulong()   { Assert.IsTrue(myClass.test_ulong() is ulong); }
+
+        [TestMethod]
+        public void return_float()   { Assert.IsTrue(myClass.test_float() is float); }
+
+        [TestMethod]
+        public void return_double()  { Assert.IsTrue(myClass.test_double() is double); }
+
+        [TestMethod]
+        public void return_decimal() { Assert.IsTrue(myClass.test_decimal() is decimal); }
+    }
+}
