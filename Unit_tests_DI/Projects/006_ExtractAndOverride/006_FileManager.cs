25a269606d39f3a161c49d9222b0e5d32e7a2513
Vik154 <Smorodin_1209@mail.ru>
2023-07-19 04:37:46 +0700

    Внедрение зависимсотей с помощью виртуальных методов

---
+++ Unit_tests_DI/Projects/006_ExtractAndOverride/006_FileManager.cs
@@ -0,0 +1,22 @@
+﻿// Внедрение зависимсотей с помощью виртуальных методов
+namespace Projects._006_ExtractAndOverride;
+
+public class FileManager {
+
+    public FileManager() { }
+
+    protected virtual List<string> FindLogFileExtension() {
+        return new FileDataObject().GetFiles();
+    }
+
+    public bool FindLogFile(string fileName) {
+        List<string> files = FindLogFileExtension();
+
+        foreach (var file in files) {
+            if (file.Contains(fileName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Unit_tests_DI/Tests/006_FileManagerTest.cs
@@ -0,0 +1,23 @@
+﻿// Внедрение зависимсотей с помощью виртуальных методов
+using Projects._006_ExtractAndOverride;
+
+namespace Tests_006 {
+
+    class FileManagerUnderTest : FileManager {
+        protected override List<string> FindLogFileExtension() {
+            return new List<string> { "file1.txt", "file2.log", "file3.exe" };
+        }
+    }
+
+    [TestFixture]
+    class FileManagerTest {
+        [Test]
+        public void FindLogFileTest10() {
+            var mgr = new FileManagerUnderTest();
+
+            var result = mgr.FindLogFile("file1.txt");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
