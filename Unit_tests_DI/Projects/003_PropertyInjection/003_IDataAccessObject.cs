8fcde05faab127b43c1bb7cecac22a4cdf0ff1ef
Vik154 <Smorodin_1209@mail.ru>
2023-07-19 02:48:23 +0700

    Техника внедрения зависимостей через свойства

---
+++ Unit_tests_DI/Projects/003_PropertyInjection/003_IDataAccessObject.cs
@@ -0,0 +1,6 @@
+﻿// Техника внедрения зависимостей через свойства
+namespace Projects._003_PropertyInjection;
+
+public interface IDataAccessObject {
+    List<string> GetFiles();
+}
---
+++ Unit_tests_DI/Tests/003_FileManagerTest.cs
@@ -0,0 +1,13 @@
+﻿// Техника внедрения зависимостей через свойства
+using Projects._003_PropertyInjection;
+namespace Tests_003;
+
+[TestFixture]
+public class FileManagerTest {
+    [Test]
+    public void FindLogFileTest3() {
+        FileManager mgr = new FileManager();
+        mgr.DataAccessObject = new StubFileDataObject();
+        Assert.IsTrue(mgr.FindLogFile("file2.log"));
+    }
+}
